b290591edff6aec4f5592859fc6ef04d498c143d
Maltemork <maltems1996@gmail.com>
2023-04-11 21:47:40 +0200

    smooth animation

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ window.addEventListener("load", start);
 
 const numArray = [
   2, 3, 16, 19, 26, 13, 3, 26, 25, 17, 18, 21, 31, 4, 12, 20, 7, 14, 27, 26, 25,
-  4, 21, 8, 16, 23, 29, 29, 23, 5, 29, 17, 9, 0, 18, 27, 29, 3, 14, 14,
+  4, 21, 8, 16, 23, 29, 29, 23, 5, 29, 17, 9, 0, 18, 27, 29, 3, 14, 14, 23,
 ];
 let num = 0;
 
@@ -17,7 +17,7 @@ function start() {
 function countFunction() {
   let newQueue = getNumberOfCustomers();
   numArray.unshift(newQueue);
-  if (numArray.length > 40) {
+  if (numArray.length > 42) {
     numArray.pop();
   }
   console.log(numArray);
@@ -34,7 +34,7 @@ function getNumberOfCustomers() {
 
 function insertIntoHTML(number) {
   document.querySelector("#grid-container").insertAdjacentHTML(
-    "beforeend",
+    "afterbegin",
     /*html*/ `
             <div class="grid-item" style="grid-row-end: ${32 - number}">
             </div>
